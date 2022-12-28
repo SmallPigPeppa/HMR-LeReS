150db6596f83d64f4e12db4383b7e1f272de3a76
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-12-28 20:28:53 +0800

    initial

--- vis_leres_pl.py
+++ vis_leres_pl.py
@@ -114,7 +114,7 @@ def strip_prefix_if_present(state_dict, prefix):
 
 if __name__ == '__main__':
     pl_ckpt_path = 'leres-ckpt-backup/last.ckpt'
-    leres_model = LeReS.load_state_dict(pl_ckpt_path)
+    leres_model = LeReS.load_from_checkpoint(pl_ckpt_path)
     depth_model = leres_model.depth_model.eval()
     image = leres_model.train_dataloader()
 
