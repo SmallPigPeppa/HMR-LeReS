0b05f78197352fdc8b6aee20484780bd18bb99cd
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-12-29 22:23:10 +0800

    initial

--- train_leres.sh
+++ train_leres.sh
@@ -12,7 +12,7 @@ python leres_train_pl.py \
 --batchsize 16 \
 --base_lr 0.001 \
 --use_tfboard \
---thread 0 \
+--thread 4 \
 --loss_mode _ranking-edge_pairwise-normal-regress-edge_msgil-normal_meanstd-tanh_pairwise-normal-regress-plane_ranking-edge-auxi_meanstd-tanh-auxi_ \
 --epoch 50 \
 --lr_scheduler_multiepochs 10 25 40 \
