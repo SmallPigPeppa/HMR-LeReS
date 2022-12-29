eb3748cbe8f8609e02b2b40fee034ea792f05f46
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-12-29 20:35:32 +0800

    initial

--- vis_leres_pl2.py
+++ vis_leres_pl2.py
@@ -146,8 +146,8 @@ if __name__ == '__main__':
     image_dir_out = 'leres_vis_out'
     image_name = 'demo-559'
     rgb=rgb[0]
-    print('depth.shape',depth.shape)
     depth=depth[0].permute(2, 0, 1)
+    print('depth.shape',depth.shape)
     pred_depth_out = depth - depth.min() + 0.01
     pred_depth = pred_depth_out.cpu().detach().numpy().squeeze()
     pred_depth_ori = cv2.resize(pred_depth, (rgb.shape[1], rgb.shape[0]))
