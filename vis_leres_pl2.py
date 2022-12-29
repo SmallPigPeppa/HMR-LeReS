eba38558620e70a6398a71559d61a78110d1a8e5
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-12-29 11:26:34 +0800

    initial

--- vis_leres_pl2.py
+++ vis_leres_pl2.py
@@ -122,8 +122,8 @@ if __name__ == '__main__':
     leres_model = LeReS.load_from_checkpoint(pl_ckpt_path)
     depth_model = leres_model.depth_model.eval()
     loader = leres_model.train_dataloader()
-    data = next(loader)
-    rgb = data['leres_loader']['rgb']
+    data = next(loader['leres_loader'])
+    rgb = data['rgb']
     depth, _ = depth_model(rgb)
 
     sf_ckpt_path = 'res50.pth'
