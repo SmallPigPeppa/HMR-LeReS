8b743532d59c5797694ef3ec2bcf4480302b8e89
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-12-29 21:14:32 +0800

    initial

--- train_leres.sh
+++ train_leres.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH="/share/wenzhuoliu/code/HMR-LeReS/HMR/src:/share/wenzhuoliu/code/HMR-LeReS/LeReS/Train"
-#export PYTHONPATH="/share/wenzhuoliu/code/HMR-LeReS/LeReS/Train"
-/share/wenzhuoliu/conda-envs/HMR-LeReS/bin/python leres_train_pl.py \
+#/share/wenzhuoliu/conda-envs/HMR-LeReS/bin/python leres_train_pl.py \
+python leres_train_pl.py \
 --smpl-mean-theta-path HMR/HMR-data/neutral_smpl_mean_params.h5 \
 --smpl-model HMR/HMR-data/neutral_smpl_with_cocoplus_reg.txt \
 --save-folder HMR/HMR-data/out-model \
@@ -9,13 +9,14 @@ export PYTHONPATH="/share/wenzhuoliu/code/HMR-LeReS/HMR/src:/share/wenzhuoliu/co
 --dataroot /share/wenzhuoliu/torch_ds/HMR-LeReS \
 --backbone resnet50 \
 --dataset_list 2020-06-11-10-06-48 \
---batchsize 2 \
+--batchsize 16 \
 --base_lr 0.001 \
 --use_tfboard \
---thread 0 \
+--thread 4 \
 --loss_mode _ranking-edge_pairwise-normal-regress-edge_msgil-normal_meanstd-tanh_pairwise-normal-regress-plane_ranking-edge-auxi_meanstd-tanh-auxi_ \
---epoch 2 \
---lr_scheduler_multiepochs 1 \
+--epoch 50 \
+--lr_scheduler_multiepochs 10 25 40 \
 --val_step 10000 \
 --snapshot_iters 100000 \
 --log_interval 1
+
