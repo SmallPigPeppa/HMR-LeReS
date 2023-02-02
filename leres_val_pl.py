c7a1e4d657defa0403aa096fbc0f0e579f2836ae
SmallPigPeppa <905328134@qq.com>
2023-02-02 21:52:24 +0800

    initial v5.0

--- leres_val_pl.py
+++ leres_val_pl.py
@@ -8,21 +8,24 @@ from pytorch_lightning.callbacks.progress import TQDMProgressBar
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 if __name__ == '__main__':
-    depth_model = LeReS()
+    # leres_model = LeReS()
+    pl_ckpt_path = 'leres-ckpt-v4.0-debug/last-v4.ckpt'
+    leres_model = LeReS.load_from_checkpoint(pl_ckpt_path)
     callbacks = []
-    wandb_logger = WandbLogger(name='leres-train-v5.0', project='HMR-LeReS-v5.0', entity='pigpeppa', offline=False)
-    wandb_logger.watch(depth_model, log="gradients", log_freq=100)
+    wandb_logger = WandbLogger(name='leres-val-v5.0', project='HMR-LeReS-v5.0', entity='pigpeppa', offline=False)
+    wandb_logger.watch(leres_model, log="gradients", log_freq=100)
     wandb_logger.log_hyperparams(train_args)
-    checkpoint_callback = ModelCheckpoint(dirpath="leres-ckpt-v5.0", save_last=True, save_top_k=2, monitor="total_loss")
+
+    checkpoint_callback = ModelCheckpoint(dirpath="leres-ckpt", save_last=True, save_top_k=2, monitor="total_loss")
 
     trainer = Trainer(
         gradient_clip_val=10.0,
-        max_epochs=100,
+        max_epochs=200,
         accelerator="gpu",
         devices=1,
+        log_every_n_steps=1,
         logger=wandb_logger,
         callbacks=[LearningRateMonitor(logging_interval="step"), TQDMProgressBar(refresh_rate=10), checkpoint_callback],
     )
 
-    trainer.fit(depth_model)
-
+    trainer.validate(leres_model)
