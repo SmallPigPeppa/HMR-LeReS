c1a596bf72b476f0b5332bcafddf84072d3f6e27
SmallPigPeppa <905328134@qq.com>
2023-02-02 21:44:54 +0800

    initial v3.0

--- leres_model_pl.py
+++ leres_model_pl.py
@@ -83,6 +83,64 @@ class LeReS(pl.LightningModule):
                    'leres_loader': leres_loader}
         return loaders
 
+    def val_dataloader(self):
+        pix_format = 'NCHW'
+        normalize = True
+        flip_prob = 0.
+        use_flip = False
+        hmr_3d_path = 'C:/Users/90532/Desktop/Datasets/HMR-LeReS/2020-06-11-10-06-48'
+        hmr_2d_path = 'C:/Users/90532/Desktop/Datasets/HMR-LeReS/2020-06-11-10-06-48'
+        hmr_mosh_path = 'C:/Users/90532/Desktop/Datasets/HMR/mosh'
+
+        # hmr_3d_path = '/share/wenzhuoliu/torch_ds/HMR-LeReS/2020-06-11-10-06-48'
+        # hmr_2d_path = '/share/wenzhuoliu/torch_ds/HMR-LeReS/2020-06-11-10-06-48'
+        # hmr_mosh_path = '/share/wenzhuoliu/torch_ds/HMR-LeReS/mosh'
+
+        use_crop = True
+        scale_range = [1.1, 2.0]
+        min_pts_required = 5
+        hmr_3d_dataset = hmr_dataset(hmr_3d_path, use_crop, scale_range, use_flip, min_pts_required, pix_format,
+                                     normalize, flip_prob)
+        hmr_2d_dataset = hmr_dataset(hmr_2d_path, use_crop, scale_range, use_flip, min_pts_required, pix_format,
+                                     normalize, flip_prob)
+        hmr_mosh_dataset = mosh_dataloader(hmr_mosh_path, use_flip, flip_prob)
+        leres_dataset = leres_gta_dataset(config.args, '2020-06-11-10-06-48')
+
+        hmr_3d_loader = DataLoader(
+            dataset=hmr_3d_dataset,
+            batch_size=config.args.batch_3d_size,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=True,
+            num_workers=config.args.num_worker
+        )
+        hmr_2d_loader = DataLoader(
+            dataset=hmr_2d_dataset,
+            batch_size=config.args.batch_size,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=True,
+            num_workers=config.args.num_worker
+        )
+        hmr_mosh_loader = DataLoader(
+            dataset=hmr_mosh_dataset,
+            batch_size=config.args.adv_batch_size,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=True,
+        )
+        leres_loader = DataLoader(
+            dataset=leres_dataset,
+            batch_size=config.args.batchsize,
+            num_workers=config.args.num_worker,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=True, )
+        loaders = {'hmr_3d_loader': hmr_3d_loader, 'hmr_2d_loader': hmr_2d_loader, 'hmr_mosh_loader': hmr_mosh_loader,
+                   'leres_loader': leres_loader}
+        combined_loaders = CombinedLoader(loaders, mode="max_size_cycle")
+        return combined_loaders
+
     def forward(self, data):
         inputs = data['rgb']
         logit, auxi = self.depth_model(inputs)
