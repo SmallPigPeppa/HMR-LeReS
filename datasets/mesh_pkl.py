79df5e9de1d352f351f39ebff00bb5f6f671c8a1
SmallPigPeppa <905328134@qq.com>
2023-02-16 22:24:06 +0800

    initial v5.0

--- datasets/mesh_pkl.py
+++ datasets/mesh_pkl.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import torch
 from torch.utils.data import Dataset, DataLoader
+
+
 class MeshDataset(Dataset):
     def __init__(self, data_dir, use_flip=True, flip_prob=0.3):
         self.data_dir = data_dir
@@ -14,9 +16,9 @@ class MeshDataset(Dataset):
     def load_data_set(self):
         print('start loading mosh data.')
         info_hmr = np.load(os.path.join(self.data_dir, 'info_hmr.pickle'), allow_pickle=True)
-        self.transl=np.array(info_hmr['transl'])
-        self.shapes = np.array(info_hmr['shape'])
-        self.poses = np.array(info_hmr['pose'])
+        self.transl = np.array(info_hmr['transl'].copy())
+        self.shapes = np.array(info_hmr['shape'].copy())
+        self.poses = np.array(info_hmr['pose'].copy())
         print('finished load mosh data, total {} samples'.format(len(self.poses)))
 
     def __len__(self):
@@ -26,7 +28,7 @@ class MeshDataset(Dataset):
         trival, pose, shape = self.transl[index], self.poses[index], self.shapes[index]
 
         if self.use_flip and random.uniform(0, 1) <= self.flip_prob:  # left-right reflect the pose
-            #todo: pose = reflect_pose(pose)
+            # todo: pose = reflect_pose(pose)
             pass
 
         return {
