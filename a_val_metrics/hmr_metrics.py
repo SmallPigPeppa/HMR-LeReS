696c188fac1b7a0dc228d4530163483e5fb45866
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-02-23 21:05:33 +0800

    initial

---
+++ a_val_metrics/hmr_metrics.py
@@ -0,0 +1,50 @@
+import torch
+
+
+def compute_mpjpe(pred, gt):
+    diff = pred - gt
+    dist = torch.norm(diff, dim=2)
+    return torch.mean(dist)
+
+
+def compute_pck(pred, gt, threshold):
+    diff = pred - gt
+    dist = torch.norm(diff, dim=2)
+    correct = (dist < threshold).sum().float()
+    return 100.0 * correct / dist.numel()
+
+
+def compute_pve(pred, gt):
+    diff = pred - gt
+    dist = torch.norm(diff, dim=2)
+    return torch.mean(dist)
+
+
+def validate_kpts_verts(pred_kpts_3d, gt_kpts_3d, pred_verts, gt_verts, pck_threshould):
+    # Remove the probability of kpts
+    gt_kpts_3d = gt_kpts_3d[:, :, :3]
+
+    # align verts
+    pred_pelvis = pred_kpts_3d[:, [0], :]
+    gt_pelvis = gt_kpts_3d[:, [0], :]
+    pred_verts_aligned = pred_pelvis - pred_pelvis
+    gt_verts_aligned = gt_verts - gt_pelvis
+
+    # compute metrics
+    mpjpe = compute_mpjpe(pred_kpts_3d, gt_kpts_3d)
+    pck = compute_pck(pred_kpts_3d, gt_kpts_3d, pck_threshould)
+    pve = compute_pve(pred_verts_aligned, gt_verts_aligned)
+
+    metrics_dict = {'mpjpe': mpjpe, 'pck': pck, 'pve': pve}
+
+    return metrics_dict
+
+
+if __name__ == '__main__':
+    pred_kpts = torch.rand([8, 23, 3])
+    gt_kpts = torch.rand([8, 23, 4])
+    gt_kpts = gt_kpts[:, :, :3]
+    a = compute_mpjpe(pred_kpts, gt_kpts)
+    b = compute_pck(pred_kpts, gt_kpts, threshold=0.2)
+    print(a)
+    # pred_vertices - pred_pelvis
---
+++ a_val_metrics/leres_metrics.py
@@ -0,0 +1,79 @@
+import torch
+import numpy as np
+
+
+def recover_metric_depth(pred, gt,):
+    a, b = np.polyfit(pred, gt, deg=1)
+    if a > 0:
+        pred_metric = a * pred + b
+    else:
+        pred_mean = np.mean(pred)
+        gt_mean = np.mean(gt)
+        pred_metric = pred * (gt_mean / pred_mean)
+    return pred_metric
+
+
+def validate_depth(pred, gt, min_threshold=0, max_threshold=15):
+    if type(pred).__module__ == torch.__name__:
+        pred = pred.cpu().numpy()
+    if type(gt).__module__ == torch.__name__:
+        gt = gt.cpu().numpy()
+    gt = np.squeeze(gt)
+    pred = np.squeeze(pred)
+    valid_mask = (gt > min_threshold) & (gt < max_threshold) & (pred>min_threshold)
+    gt = gt[valid_mask]
+    pred = pred[valid_mask]
+
+    criteria = {}
+
+    # Scale matching
+    pred = recover_metric_depth(pred, gt)
+
+    n_pxl = gt.size
+
+    # Mean Absolute Relative Error
+    rel = np.abs(gt - pred)
+    abs_rel_sum = np.sum(rel)
+    criteria['err_absRel'] = abs_rel_sum / n_pxl
+
+    # WHDR error
+    whdr_err_sum, eval_num = weighted_human_disagreement_rate(gt, pred)
+    criteria['err_whdr'] = whdr_err_sum / eval_num
+    return criteria
+
+
+def weighted_human_disagreement_rate(gt, pred):
+    p12_index = select_index(gt)
+    gt_reshape = np.reshape(gt, gt.size)
+    pred_reshape = np.reshape(pred, pred.size)
+    mask = gt > 0
+    gt_p1 = gt_reshape[mask][p12_index['p1']]
+    gt_p2 = gt_reshape[mask][p12_index['p2']]
+    pred_p1 = pred_reshape[mask][p12_index['p1']]
+    pred_p2 = pred_reshape[mask][p12_index['p2']]
+
+    p12_rank_gt = np.zeros_like(gt_p1)
+    p12_rank_gt[gt_p1 > gt_p2] = 1
+    p12_rank_gt[gt_p1 < gt_p2] = -1
+
+    p12_rank_pred = np.zeros_like(gt_p1)
+    p12_rank_pred[pred_p1 > pred_p2] = 1
+    p12_rank_pred[pred_p1 < pred_p2] = -1
+
+    err = np.sum(p12_rank_gt != p12_rank_pred)
+    valid_pixels = gt_p1.size
+    return err, valid_pixels
+
+
+def select_index(gt_depth, select_size=10000):
+    valid_size = np.sum(gt_depth > 0)
+    try:
+        p = np.random.choice(valid_size, select_size * 2, replace=False)
+    except:
+        p = np.random.choice(valid_size, select_size * 2 * 2, replace=True)
+    np.random.shuffle(p)
+    p1 = p[0:select_size * 2:2]
+    p2 = p[1:select_size * 2:2]
+
+    p12_index = {'p1': p1, 'p2': p2}
+    return p12_index
