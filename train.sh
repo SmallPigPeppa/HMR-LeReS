d7c89a1c4a5aac964acfd7134acf8f2613bdc920
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-01 16:24:15 +0800

    initial

--- train.sh
+++ train.sh
@@ -1,17 +1,17 @@
 # conda activate HMR-LeReS3
-export PYTHONPATH="/mnt/mmtech01/usr/liuwenzhuo/code/HMR-LeReS-new/HMR/src:/mnt/mmtech01/usr/liuwenzhuo/code/HMR-LeReS-new/LeReS/Train"
-python h_main_train_cluster.py \
+export PYTHONPATH="/share/wenzhuoliu/code/HMR-LeReS-new/HMR/src:/share/wenzhuoliu/code/HMR-LeReS-new/LeReS/Train"
+python main.py \
 --smpl-mean-theta-path HMR/HMR-data/neutral_smpl_mean_params.h5 \
 --smpl-model HMR/HMR-data/neutral_smpl_with_cocoplus_reg.txt \
---gta_dataset_dir /mnt/mmtech01/dataset/wenzhuoliu/HMR-LeReS/2020-06-11-10-06-48 \
---mesh_dataset_dir /mnt/mmtech01/dataset/wenzhuoliu/HMR-LeReS/mesh \
+--gta_dataset_dir /share/wenzhuoliu/torch_ds/HMR-LeReS/2020-06-11-10-06-48 \
+--mesh_dataset_dir /share/wenzhuoliu/torch_ds/HMR-LeReS/mesh \
 --ckpt_dir hmr-leres-ckpt \
 --name hmr-leres-cluster \
 --project HMR-LeReS-v6.0 \
 --entity pigpeppa \
 --num_gpus 2 \
---max-epochs 200 \
---num-workers 0 \
+--max-epochs 100 \
+--num-workers 4 \
 --batch-size 16 \
 --eval-batch-size 16 \
 --adv-batch-size 48 \
