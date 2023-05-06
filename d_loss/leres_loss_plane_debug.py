c7b91d1ddacaac5b7bb2f2b9364a7769cc9f2f3b
lwz <905328134@qq.com>
2023-05-06 12:05:54 +0800

    initial

--- d_loss/leres_loss_plane_debug.py
+++ d_loss/leres_loss_plane_debug.py
@@ -226,6 +226,7 @@ class PWNPlanesLoss(pl.LightningModule):
         loss = torch.tensor(0.0).to(self.device)
         valid_depth_masks = (gt_depth > self.min_threshold) & (gt_depth < self.max_threshold)
         plane_mask = plane_mask * (valid_depth_masks.float().to(self.device))
+        print('plane_mask.shape:',plane_mask.shape)
         valid_planes_num = 0
         for i in range(B):
             self.fx = focal_length[i] if focal_length is not None else 256.0
