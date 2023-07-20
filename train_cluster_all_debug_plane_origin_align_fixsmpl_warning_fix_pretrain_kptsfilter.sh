d34071d9d26843e318972a9112083a7bbf40892e
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-07-21 00:38:33 +0800

    initial

--- train_cluster_all_debug_plane_origin_align_fixsmpl_warning_fix_pretrain_kptsfilter.sh
+++ train_cluster_all_debug_plane_origin_align_fixsmpl_warning_fix_pretrain_kptsfilter.sh
@@ -1,12 +1,12 @@
 # conda activate hmr-leres
 export PYTHONPATH="./HMR/src:./LeReS/Train"
-/root/miniconda3/envs/hmr-leres/bin/python main_all_plane_origin_align_fix_pretrain.py \
+/root/miniconda3/envs/hmr-leres/bin/python main_all_plane_origin_align_fix_pretrain_kptsfilter.py \
 --smpl-mean-theta-path HMR/HMR-data/neutral_smpl_mean_params.h5 \
 --smpl-model HMR/HMR-data/neutral_smpl_with_cocoplus_reg.txt \
 --gta_dataset_dir /mnt/mmtech01/dataset/vision_text_pretrain/gta-im \
 --mesh_dataset_dir /mnt/mmtech01/dataset/vision_text_pretrain/gta-im \
---ckpt_dir hmr-leres-ckpt-debug/v8.0-fix-pretrain-6 \
---name fix-pretrain-6 \
+--ckpt_dir hmr-leres-ckpt-debug/v8.0-fix-pretrain-kptsfilter \
+--name fix-pretrain-kptsfilter \
 --project HMR-LeReS-v8.0 \
 --entity pigpeppa \
 --num_gpus 8 \
