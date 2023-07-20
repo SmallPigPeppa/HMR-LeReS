c6b08fb749cb9810f7f26f3fa7590a687250940f
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-07-21 00:41:15 +0800

    initial

--- d_make_leres_dataset/cp.sh
+++ d_make_leres_dataset/cp.sh
@@ -3,7 +3,7 @@
 SRC_DIR="/mnt/mmtech01/usr/liuwenzhuo/code/HMR-LeReS-new-rm-crop/d_make_leres_dataset/plane_mask"
 DST_DIR="/mnt/mmtech01/dataset/vision_text_pretrain/gta-im/FPS-5-addmask"
 
-for dir in $(ls $SRC_DIR)
+for dir in $(ls $DST_DIR)
 do
     cp -r $SRC_DIR/$dir/* $DST_DIR/$dir/
 done
